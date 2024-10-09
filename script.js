989bfe21e0a68cc5e6c2eb1ee7b8a3aa474a4ce2
Cisz <cisz13h@gmail.com>
2024-10-09 08:33:41 -0300

    Function playRockPaperScissor adicionada

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 /* Desenvolva seu código abaixo */
+
+function playRockPaperScissor(player1, player2){
+    let result = "";
+
+    if (player1 === "Papel" && player2 === "Pedra") {
+        result = "Jogador 1 venceu!"
+        
+    } else if (player1 === "Pedra" && player2 === "Tesoura") {
+        result  = "Jogador 1 venceu!"
+
+    } else if (player1 === "Tesoura" && player2 === "Papel") {
+        result  = "Jogador 1 venceu!"
+
+    } else if (player1 === "Papel" && player2 === "Tesoura") {
+        result  = "Jogador 2 venceu!"
+
+    } else if (player1 === "Tesoura" && player2 === "Pedra") {
+        result  = "Jogador 2 venceu!"
+
+    } else if (player1 === "Pedra" && player2 === "Papel") {
+        result = "Jogador 2 venceu!"
+        
+    } else result  = "Empate!"
+
+    return result
+}
